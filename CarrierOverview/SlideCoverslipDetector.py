439d7bd5ca29bd4026e07c517e30bd05a760f8fb
Fabrice Cordelières <fabricecordelieres@users.noreply.github.com>
2021-03-22 15:34:59 +0100

    Added the find_circular_coverslips_in_containers function in detector class

--- CarrierOverview/SlideCoverslipDetector.py
+++ CarrierOverview/SlideCoverslipDetector.py
@@ -8,7 +8,7 @@ class SlideCoverslipDetector:
     """
 
     coo = ''
-    mask=''
+    mask = ''
     containers = []
     coverslips = []
 
@@ -24,16 +24,21 @@ class SlideCoverslipDetector:
         img = self.coo.get_image()
         bilateral_filter = cv2.bilateralFilter(img, 17, 9, 36)
         self.mask = cv2.adaptiveThreshold(bilateral_filter, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
-                                             255, 0)
+                                          255, 0)
 
     def find_containers(self, width=40, height=86, tolerance=0.05):
         """
         Performs containers detection: rectangle of user-defined width and height will be looked for,
-        taking into account the input tolerance (values +/- the tolerance in percents)
+        taking into account the input tolerance (values +/- the tolerance in percents). It returns a dictionary
+        containing the found containers and appends them to the self.container class attribute (list).
         :param width: the expected container width in mm (default value: 40mm, corresponding to the 3 slides holder)
         :param height: the expected container height in mm (default value: 86mm, corresponding to the 3 slides holder)
         :param tolerance: tolerance for dimensions expressed in percents (default value: 0.05)
+        :return a dictionary containing the found containers
         """
+        index = 1
+        out = dict()
+
         width = width * 1000 / self.coo.metadata['CalibX_microns']
         height = height * 1000 / self.coo.metadata['CalibY_microns']
 
@@ -49,35 +54,89 @@ class SlideCoverslipDetector:
             if width * (1 - tolerance) < w < width * (1 + tolerance) and height * (1 - tolerance) < h < height * (
                     1 + tolerance):
                 self.containers.append(bbox)
+                out['Container' + str(index)] = {'Coordinates': list(bbox)}
+                index = index + 1
 
-    def find_circular_coverslips(self, diameter=18, tolerance=0.1):
+        return out
+
+    def find_circular_coverslips(self, diameter=18, tolerance=0.1, limit_to_area = None):
         """
         Performs coverslips detection: rectangle of user-defined diameter will be looked for,
-        taking into account the input tolerance (values +/- the tolerance in percents)
+        taking into account the input tolerance (values +/- the tolerance in percents). It returns a dictionary
+        containing the found coverslips and appends them to the self.coverslips class attribute (list).
         :param diameter: the expected circular coverslip's diameter in mm (default value: 18mm)
         :param tolerance: tolerance for dimensions expressed in percents (default value: 0.1)
+        :param limit to area: limits the detection of the coverslips to a certain image area
+        :return a dictionary containing the found coverslips
         """
-        radius = (diameter * 1000 / self.coo.metadata['CalibX_microns']) /2
+        index = 1
+        out = dict()
+        radius = (diameter * 1000 / self.coo.metadata['CalibX_microns']) / 2
 
         if self.mask is not None:
             self.preprocess_image()
 
-        circles = cv2.HoughCircles(self.mask, cv2.HOUGH_GRADIENT, 2, self.mask.shape[1] / 10,
-                                   param1=100, param2=65,
-                                   minRadius=int(radius*(1-tolerance)), maxRadius=int(radius*(1+tolerance)))
+        if limit_to_area is None:
+            circles = cv2.HoughCircles(self.mask, cv2.HOUGH_GRADIENT, 2, self.mask.shape[1] / 10,
+                                       param1=100, param2=65,
+                                       minRadius=int(radius * (1 - tolerance)), maxRadius=int(radius * (1 + tolerance)))
+        else:
+            x, y, w, h = limit_to_area
+            circles = cv2.HoughCircles(self.mask[y:y + h, x:x + w], cv2.HOUGH_GRADIENT, 2, self.mask.shape[1] / 10,
+                                       param1=100, param2=65,
+                                       minRadius=int(radius * (1 - tolerance)), maxRadius=int(radius * (1 + tolerance)))
 
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for circle in circles[0, :]:
+                if limit_to_area is not None:
+                    x, y, w, h = limit_to_area
+                    circle[0]= circle[0] + x
+                    circle[1] = circle[1] + y
                 self.coverslips.append(circle)
+                out['Coverslip'+str(index)] = list(circle)
+                index = index +1
+
+        return out
+
+    def find_circular_coverslips_in_containers(self, width_container=40, height_container=86, tolerance_container=0.05, diameter_coverslip=18, tolerance_coverslip=0.1):
+        """
+        Performs coverslips detection: rectangle of user-defined diameter will be looked for,
+        taking into account the input tolerance (values +/- the tolerance in percents)
+        :param width_container: the expected container width in mm (default value: 40mm, corresponding to the 3 slides holder)
+        :param height_container: the expected container height in mm (default value: 86mm, corresponding to the 3 slides holder)
+        :param tolerance_container: tolerance for dimensions expressed in percents (default value: 0.05)
+        :param diameter_coverslip: the expected circular coverslip's diameter in mm (default value: 18mm)
+        :param tolerance_coverslip: tolerance for dimensions expressed in percents (default value: 0.1)
+        :return a dictionary containing the found containers and coverslips taking this shape:
+                {Container1:
+                    {'Coordinates': [x, y, w, h],
+                    'Coverslips': {'Coverslip1': [w, y, radius],
+                                   'Coverslip2': [w, y, radius]},
+                    }
+                 Container2:
+                    {'Coordinates': [x, y, w, h],
+                    'Coverslips': {'Coverslip1': [w, y, radius],
+                                   'Coverslip2': [w, y, radius]}
+                    }
+                }
+        """
+
+        out = self.find_containers(width_container, height_container, tolerance_container)
+
+        for key, value in out.items():
+            coverslips = self.find_circular_coverslips(diameter_coverslip, tolerance_coverslip, limit_to_area=value['Coordinates'])
+            out[key]['Coverslips']=coverslips
 
     def get_image(self):
         """
         Generates and returns an image superimposing all detections (containers and coverslips)
-        over the original image
+        over the original image. NB: locations for containers/coverslips are taken from the class attributes, with no
+        aspect of hierarchy between them. Detection should have been performed BEFORE calling get_image in order to feed
+        the two class attributes.
         :return: the original image overlaid with all detections
         """
-        img=cv2.cvtColor(self.coo.get_image(), cv2.COLOR_GRAY2RGB)
+        img = cv2.cvtColor(self.coo.get_image(), cv2.COLOR_GRAY2RGB)
 
         for container in self.containers:
             x, y, w, h = container  # unpacking
@@ -102,7 +161,6 @@ class SlideCoverslipDetector:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-
     def test_detect(self):
         img = self.coo.get_image()
         img_out = cv2.cvtColor(img.copy(), cv2.COLOR_GRAY2RGB)
@@ -125,7 +183,6 @@ class SlideCoverslipDetector:
                 # circle outline
                 radius = i[2]
                 cv2.circle(img_out, center, radius, (255, 0, 0), 5)
-                print(i)
         # -----------------------------------------------------
         slides, hierarchy = cv2.findContours(adaptive_thr, cv2.RETR_LIST,
                                              cv2.CHAIN_APPROX_SIMPLE)  # Only want the contours, not the hierarchy
@@ -135,7 +192,6 @@ class SlideCoverslipDetector:
             x, y, w, h = bbox  # unpacking
             if 800 < w < 1000 and 1800 < h < 2000:
                 cv2.rectangle(img_out, (x, y), (x + w, y + h), (0, 255, 0), 5)
-                print(bbox)
 
         zoom = 0.15
         w = int(img.shape[1] * zoom)
--- CarrierOverview/test_SlideCoverslipDetector_class.py
+++ CarrierOverview/test_SlideCoverslipDetector_class.py
@@ -6,7 +6,12 @@ filename = 'CarrierOverview.czi'
 co_obj = co(input_dir, filename)
 sc_obj = sc(co_obj)
 #sc_obj.test_detect()
-sc_obj.find_containers() # Options are available: width, height and tolerance
-sc_obj.find_circular_coverslips() # Options are available: diameter and tolerance
+#sc_obj.find_containers() # Options are available: width, height and tolerance
+#print(sc_obj.find_circular_coverslips(limit_to_area=(250, 0, 1250, 2030))) # Options are available: diameter and tolerance
+#sc_obj.find_circular_coverslips(diameter= 20, tolerance=.2)
+#sc_obj.find_circular_coverslips_in_containers(diameter_coverslip= 20, tolerance_coverslip=.2)
+
+sc_obj.find_circular_coverslips_in_containers()
+
 sc_obj.show()
 
